01eca744e143aeb0fb1ca1fb98e89d83c99b6323
abhinavmabhi <dexmarketing192018@gmail.com>
2025-12-03 16:38:43 +0530

    commit

--- app/views.py
+++ app/views.py
@@ -1,49 +1,56 @@
-from django.shortcuts import render
-from .models import Products,Gallery,HomeBanner
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Products, Gallery, HomeBanner
+
 
 def home(request):
-    banner=HomeBanner.objects.order_by("-id").first()
-    products = Products.objects.order_by("-id")[:8]
-    context = {
-        'products': products,
-        'banner':banner
-        }
-    return render(request, 'home.html',context)
+    banner = HomeBanner.objects.only("image1", "image2").order_by("-id").first()
 
+  
+    products = Products.objects.only(
+        "id", "name", "price", "image","image2"
+    ).order_by("-id")[:8]
+
+    return render(request, "home.html", {
+        "products": products,
+        "banner": banner,
+    })
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
 
 
 
 def products(request):
-    products = Products.objects.order_by("-id")
-    context = {
-        'products': products
-        }
-    return render(request, 'products.html',context)
+   
+    products = Products.objects.only("id", "name", "price", "image").order_by("-id")
 
-def Product_details(request,id):
-    product=Products.objects.get(id=id)
-    context={
-        'product':product
-        }
-    return render(request,'product_details.html',context)
+    return render(request, "products.html", {
+        "products": products
+    })
+
+
+def Product_details(request, id):
+  
+    product = get_object_or_404(Products, id=id)
+
+    return render(request, "product_details.html", {
+        "product": product
+    })
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
 
 
 
 def gallery(request):
-    gallery_items = Gallery.objects.order_by("-id")
-    context = {
-        'gallery': gallery_items
-        }
-    return render(request, 'gallery.html',context)
+    gallery_items = Gallery.objects.only("id", "title", "image").order_by("-id")
+
+    return render(request, "gallery.html", {
+        "gallery": gallery_items
+    })
+
 
 def privacy_policy(request):
-    return render(request, 'privacy_policy.html')
+    return render(request, "privacy_policy.html")
